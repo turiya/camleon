73ce4bedc78b38050d32754a0234ef79076785bc
matt.zukowski <matt.zukowski@41e4064f-be21-0410-9b72-47a42af0d930>
2006-11-13 03:00:45 +0000

    more fft work

--- trunk/fft.ml
+++ trunk/fft.ml
@@ -2,10 +2,11 @@
 
 open Bigarray;;
 open Printf;;
+open List;;
 
 let complex a b = { Complex.re = a; Complex.im = b }
 
-let n = 256;;
+let n = 65536;;
 let fft = Fftw.r_create Fftw.forward n ~normalize:true;;
 
 let pi = 4. *. atan 1.;;
@@ -16,22 +17,30 @@ let convert_to_decibels a =
 let normalize a =
   log a;;
 
-let sampling_rate = 44100;;
+let sampling_rate = 22050.;;
 
 let sinusoid a freq i =
-  amp *. (cos (sampling_rate *. a *. freq *. (float_of_int i) *. pi *. 2.));;
+  a *. (cos (a *. (freq /. sampling_rate) *. (float_of_int i) *. pi *. 2.));;
    
+let spectralize data =
+	let fa = fft data in
+  let spectro = [] in
+	for i = 0 to (n - 1) do
+		spectro @ [(
+				(sampling_rate *. float_of_int i) /. float_of_int n,
+				abs_float fa.{i}
+			)] 
+	done;
 
 let () =
   let a = Array1.create float64 c_layout n in
   let () =
     for i = 0 to n - 1 do
-      a.{i} <- (cos ((float_of_int i) *. (1. /. 2.) *. pi *. 2.))
-        +. (cos ((float_of_int i) *. (1. /. 8.) *. pi *. 2.))
-        +. (1.0 *. (cos ((float_of_int i) *. (1. /. 4.) *. pi *. 2.)))
+      a.{i} <- (sinusoid 1. 1000. i) +.
+				(sinusoid 1. 5000. i) +. (sinusoid 1. 10000. i) +. (sinusoid 1. 20. i)
     done in
   let fa = fft a in
     for i = 0 to (n - 1) do
-      printf "%f, %f\n" (float_of_int i /. float_of_int n) ((abs_float fa.{i}))
+      printf "%f, %f\n" ((sampling_rate *. float_of_int i) /. float_of_int n) ((abs_float fa.{i}))
 			(*printf "%i, %f\n" i a.{i}*)
     done;
