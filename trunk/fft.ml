57ec9a1466599dde229a3bc4e29654bf24a15a7f
matt.zukowski <matt.zukowski@41e4064f-be21-0410-9b72-47a42af0d930>
2006-11-15 03:58:06 +0000

    yayvim fft.ml

--- trunk/fft.ml
+++ trunk/fft.ml
@@ -23,43 +23,49 @@ let sampling_rate = 44100.;;
 let generate_sinusoid_sample a freq i =
   a *. (cos (a *. (freq /. sampling_rate) *. (float_of_int i) *. pi *. 2.));;
 
+(* rounds a float to the nearest integer *)
+let round f =
+	if (f -. (floor f)) >= 0.5 then
+		int_of_float (ceil f)
+	else
+		int_of_float (floor f);;
+
 (* return the index of the spectral bucket that sample i would
 	fall into  when the total number of buckets is n and the total
 	number of samples is s *)
-let which_bucket i s n = 
-  ((floor (float_of_int i /. float_of_int n) *. (s /. n)) ;;
+let which_bucket s n i = 
+	let i = float_of_int i in
+	let s = float_of_int s in
+	let n = float_of_int n in
+  int_of_float ((floor ((i *. n) /. s)) );;
 	(*int_of_float (floor (sqrt ((float_of_int i) *. (float_of_int n))));;*)
 	
-let rec freq_bucket linear_spectrum i n =
-	let s = Array1.dim linear_spectrum in
-	let b = which_bucket i n in
-	if b != which_bucket (i+1) n then
-		linear_spectrum.{b}
-	else
-		linear_spectrum.{b} +. abs_float (freq_bucket linear_spectrum (i+1) n);;
+let rec bucketize spectrum buckets =
+	let n = Array1.dim buckets in
+	let s = Array1.dim spectrum in
+	let buck = which_bucket s n in
+	for i = 0 to s - 1 do
+		let b = buck i in
+		buckets.{b} <- buckets.{b} +. abs_float spectrum.{i}
+	done;;
 
-let spectralize data spectrum ?(min=0) ?(max=(Array1.dim data)-1) =
-  let n = Array1.dim spectrum in
-  let linear_spectrum = fft data in(*Array1.sub (fa) min (max - min) in*)
+let clear_buckets buckets =
+	for i = 0 to (Array1.dim buckets) - 1 do buckets.{i} <- 0. done;;
+
+let spectralize data ?(min=0) ?(max=(Array1.dim data)-1) buckets =
+	clear_buckets buckets;
+  let n = Array1.dim buckets in
+  let spectrum = fft data in(*Array1.sub (fa) min (max - min) in*)
+	Printf.printf "spectrum has %n buckets\n" n;
 	(* now we get logarithmic! *)
-  for i = 0 to n - 1 do
-    spectrum.{i} <- freq_bucket linear_spectrum i n
-  done;;
+  bucketize spectrum buckets;;
   
 let print_spectrum spectrum =
 	let n = Array1.dim spectrum in
 	for i = 0 to n - 1 do
 		Printf.printf "%f\t%f\n" ((sampling_rate *. float_of_int i) /. float_of_int n) spectrum.{i};
 	done;;
-
-
   
-let print_tuple (x,y) =
-  printf "%f , %f\n" x y;;
-
-let print_data data =
-  List.iter print_tuple data;;
-
 (*let () =
   let a = Array1.create float64 c_layout n in
   let () =
