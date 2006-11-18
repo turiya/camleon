2288e4301898bf3a83c28e42e0b7bec71518b4a2
matt.zukowski <matt.zukowski@41e4064f-be21-0410-9b72-47a42af0d930>
2006-11-18 16:43:50 +0000

    using variant types for arguments

--- trunk/fft.ml
+++ trunk/fft.ml
@@ -18,10 +18,10 @@ let convert_to_decibels a =
 let normalize a =
   log a;;
 
-let sampling_rate = 44100.;;
+let `SamplesPerSecond sr = 44100.;;
 
-let generate_sinusoid_sample a freq i =
-  a *. (cos (a *. (freq /. sampling_rate) *. (float_of_int i) *. pi *. 2.));;
+let generate_sinusoid_sample a `Hz freq i =
+  a *. (cos (a *. (freq /. sr) *. (float_of_int i) *. pi *. 2.));;
 
 (* rounds a float to the nearest integer *)
 let round f =
@@ -30,26 +30,35 @@ let round f =
 	else
 		int_of_float (floor f);;
 
-let index_of_frequency freq sampling_rate n =
-  int_of_float (floor ((freq *. (float_of_int n)) /. sampling_rate));;
+let index_of_frequency `Hz freq `SamplesPerSecond sr n =
+  int_of_float (floor ((freq *. (float_of_int n)) /. sr));;
 
-let frequency_of_index i sampling_rate n =
-  ((sampling_rate *. float_of_int i) /. float_of_int n);;
+let frequency_of_index `SampleIndex i `SamplesPerSecond sr n =
+  ((sr *. float_of_int i) /. float_of_int n);;
 
 (* return the index of the spectral bucket that sample i would
 	fall into  when the total number of buckets is n and the total
 	number of samples is s *)
-let which_bucket s n i = 
+let bucket_of_index `SampleCount s `BucketCount n `SampleIndex i = 
 	let i = float_of_int i in
 	let s = float_of_int s in
 	let n = float_of_int n in
-  int_of_float ((floor ((i *. n) /. s)) );;
+  `BucketIndex int_of_float ((floor ((i *. n) /. s)));;
 	(*int_of_float (floor (sqrt ((float_of_int i) *. (float_of_int n))));;*)
+
+let index_of_bucket `SampleCount s `BucketCount n `BucketIndex b =
+  let b = float_of_int b in
+  let s = float_of_int s in
+  let n = float_of_int n in
+  `SampleIndex int_of_float ((floor ((b *. s) /. n)));;
+
+let frequency_of_bucket `SampleCount s `BucketCount n `BucketIndex b `SamplesPerSecond sr =
+  `Hz frequency_of_index (index_of_bucket s n b) sr n;;
 	
 let rec bucketize spectrum buckets =
-	let n = Array1.dim buckets in
-	let s = Array1.dim spectrum in
-	let buck = which_bucket s n in
+	let n = `BucketCount Array1.dim buckets in
+	let s = `SampleCount Array1.dim spectrum in
+	let buck = bucket_of_index s n in
 	for i = 0 to s - 1 do
 		let b = buck i in
 		buckets.{b} <- buckets.{b} +. abs_float spectrum.{i}
@@ -67,10 +76,10 @@ let spectralize data ?(min=18.) ?(max=20000.) buckets =
     ((index_of_frequency max sampling_rate s) - (index_of_frequency min sampling_rate s)) in
   bucketize spectrum buckets;;
   
-let print_spectrum ?(min=18.) ?(max=20000.) buckets =
+let print_spectrum ?(min=18.) ?(max=20000.) buckets s =
 	let n = Array1.dim buckets in
-	for i = 0 to n - 1 do
-		Printf.printf "%f\t%f\n" (frequency_of_index i sampling_rate n ~min:min ~ax:max) spectrum.{i};
+	for b = 0 to n - 1 do
+		Printf.printf "%f\t%f\n" (frequency_of_bucket s b n sampling_rate n ~min:min ~ax:max) spectrum.{i};
 	done;;
   
 (*let () =
