f10f54cca35871fdb1bd288b5ebc43a2c8d587fc
matt.zukowski <matt.zukowski@41e4064f-be21-0410-9b72-47a42af0d930>
2006-12-04 01:27:09 +0000

    pretty much fully working spectralizer and spectroscope

--- scopes.ml
+++ scopes.ml
@@ -1,4 +1,5 @@
-open Bigarray;;
+open Bigarray
+open Printf
 
 module Scopes =
 struct
@@ -25,15 +26,52 @@ class oscilloscope ?(barsize=80) ?(maxval=32767) () =
 
 		method bar = bar
 
+		(* the default transformer here assumes that incoming values are floats in the range 0. to 1. *)
+		method print_data ?(transform=fun v -> int_of_float (32767. *. v)) (data : (float, Bigarray.float64_elt, Bigarray.c_layout) Bigarray.Array1.t) =
+			for i = 0 to Array1.dim data - 1 do
+				self#set (transform data.{i});
+				Printf.printf "%s\n" self#bar
+			done
+
 	end;;
 
-(*class spectroscope spectrum ?(minfreq=0.) ?(maxfreq=1.) ?(width=80)=
+class spectroscope ?(barsize=80) ?(maxval=1.) ~freq_map spectrum =
 	object (self)
-		val spectrum = spectrum;;
-		val display = Array.make (Array1.dim spectrum) (String.create (width + 7));;
+		val display = Array.init (Array.length freq_map) (fun i -> ((Printf.sprintf "%7.1f " freq_map.(i)) ^ (String.make (barsize) ' ') ^ "]"))
+		val display_width = barsize + 8 + 1
+
+		method clear_display =
+			for i = 0 to Array.length display - 1 do
+				String.fill display.(i) 8 (barsize) ' '
+			done
+
+		method init_bar i =
+			(Printf.sprintf "%7.1f " freq_map.(i)) ^ (String.make (barsize) ' ')
 
 		method update = 
-			for i = 0 to Array.dim 
-	*)		
+			for i = 0 to Array1.dim spectrum - 1 do
+				try 
+					String.fill display.(i) 8 (self#calc_width spectrum.{i}) '%'
+				with Invalid_argument "Val_out_of_range" -> String.fill display.(i) 8 barsize '!'
+			done
+	
+		method calc_width v =
+			if (v > maxval) then
+				invalid_arg "Val_out_of_range"
+			else
+				int_of_float ((v /. maxval) *. float_of_int barsize)
 
+		method print_freq_map =
+			for i = 0 to Array.length freq_map - 1 do
+				Printf.printf "%7.1f\n" freq_map.(i)
+			done
+
+		method print =
+			self#clear_display;
+			self#update;
+			for i = 0 to Array.length display - 1 do
+				Printf.printf "%s\n" display.(i)
+			done
+
+	end;;
 end;;
