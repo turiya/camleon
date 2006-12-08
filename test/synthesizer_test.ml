6b24ab17148f34f1dc8a579313133b9f82ef455c
matt.zukowski <matt.zukowski@41e4064f-be21-0410-9b72-47a42af0d930>
2006-12-08 05:25:45 +0000

    trying to fix tests...

--- test/synthesizer_test.ml
+++ test/synthesizer_test.ml
@@ -19,13 +19,20 @@ let test_generate_sinusoid_sample_for_multiple_frequencies _ =
 	assert_equal ~cmp:(cmp_float_abs) ~printer:(string_of_float) 0. (synth#generate_sinusoid_sample_for_multiple_frequencies ~freqs:[1.;2.] 10)
 
 let test_generate_sinusoid_samples _ =
-	assert_equal ~printer:(string_of_int) 1000 (Array1.dim (synth#generate_sinusoid_samples ~n:1000 [1.;2.;0.5]));
-	let data = synth#generate_sinusoid_samples ~n:100 [1.] in
+	assert_equal ~printer:(string_of_int) 8192 (Array1.dim (synth#generate_sinusoid_samples [1.;2.;0.5]));
+	let data = synth#generate_sinusoid_samples [1.] in
 	assert_equal ~cmp:(cmp_float_abs) ~printer:(string_of_float) (-1.) data.{10}
+
+let test_generate_sinusoid_samples_into_array _ =
+	let data = Array1.create float64 c_layout 1000 in
+	synth#generate_sinusoid_samples ~data:data [1.];
+	assert_equal ~cmp:(cmp_float_abs) ~printer:(string_of_float) (-1.) data.{10}
+
 
 let suite = "synthesizer tests" >::: ["test_generate_sinusoid_sample" >:: test_generate_sinusoid_sample;
 																			"test_generate_sinusoid_sample_for_multiple_frequencies" >:: test_generate_sinusoid_sample_for_multiple_frequencies;
-																			"test_generate_sinusoid_samples" >:: test_generate_sinusoid_samples]
+																			"test_generate_sinusoid_samples" >:: test_generate_sinusoid_samples;
+																			"test_generate_sinusoid_samples_into_array" >:: test_generate_sinusoid_samples_into_array]
 
 let _ =
 	run_test_tt ~verbose:false suite
