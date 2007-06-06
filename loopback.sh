192abf669c3ab6456c0185e98eec3e53822d9d33
matt.zukowski <matt.zukowski@41e4064f-be21-0410-9b72-47a42af0d930>
2007-06-06 19:17:19 +0000

    added preliminary Makefile

--- loopback.sh
+++ loopback.sh
@@ -1,1 +1,1 @@
-ecasound -a 1,2 -i test.mp3 -a:1 -o alsa -a:2 -o stdout | ocaml unix.cma scopes.cma pipein.ml
+ecasound -a 1,2 -i test.wav -a:1 -o alsa -a:2 -o stdout | ocaml unix.cma scopes.cma pipein.ml
